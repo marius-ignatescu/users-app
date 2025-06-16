6e9be62e5e99160352743700b4aa72a0c6439db5
Marius Ignatescu <marius@descopera.org>
2025-06-16 14:27:16 +0300

    Display context menu based on current route

--- users/src/app/components/contextual-navigation-bar/contextual-navigation-bar.ts
+++ users/src/app/components/contextual-navigation-bar/contextual-navigation-bar.ts
@@ -1,11 +1,31 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-contextual-navigation-bar',
-  imports: [],
+  imports: [NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './contextual-navigation-bar.html',
   styleUrl: './contextual-navigation-bar.css'
 })
+
 export class ContextualNavigationBar {
-  title = signal('Test');
+  currentRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  getPageTitle(route: string): string {
+    switch (route) {
+      case '/user':
+        return 'User Management';
+      case '/settings':
+        return 'Settings';
+      default:
+        return 'Dashboard';
+    }
+  }
 }
