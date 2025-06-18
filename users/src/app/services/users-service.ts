edff3bfbaaaf1942c8a5e4752501aedcecb749c7
Marius Ignatescu <marius@descopera.org>
2025-06-18 13:37:55 +0300

    Moved the API url from service to a environment file.

--- users/src/app/services/users-service.ts
+++ users/src/app/services/users-service.ts
@@ -1,25 +1,26 @@
 import { inject, Injectable } from '@angular/core';
 import { UserItem } from '../model/user.type';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   http = inject(HttpClient);
-  private url = `https://jsonplaceholder.typicode.com/users`
+  private apiUrl = environment.apiUrl;
 
   constructor() { }
 
   getUsers(){
-    return this.http.get<Array<UserItem>>(this.url);
+    return this.http.get<Array<UserItem>>(`${this.apiUrl}/users`);
   }
 
   getUserByID(id: number){
-    return this.http.get(`${this.url}/${id}`);
+    return this.http.get(`${this.apiUrl}/users/${id}`);
   }
 
   deleteUser(id: UserItem){
-    return this.http.delete(`${this.url}/${id}`);
+    return this.http.delete(`${this.apiUrl}/users/${id}`);
   }
 }
