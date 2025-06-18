3106db3b3e8c41c0342b90b1ee8bc869cfbdc7d4
Marius Ignatescu <marius@descopera.org>
2025-06-18 16:36:19 +0300

    Changed table in user details to use cdk table

--- users/src/app/pages/user/user-details/user-details.ts
+++ users/src/app/pages/user/user-details/user-details.ts
@@ -4,10 +4,11 @@ import { NgIf } from '@angular/common';
 import { UsersService } from '../../../services/users-service';
 import { ContextualNavigationBar } from "../../../components/contextual-navigation-bar/contextual-navigation-bar";
 import { RouterModule } from '@angular/router';
+import { CdkTable, CdkTableModule } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-user-details',
-  imports: [NgIf, ContextualNavigationBar, RouterModule],
+  imports: [CdkTable, CdkTableModule, NgIf, ContextualNavigationBar, RouterModule],
   templateUrl: './user-details.html',
   styleUrl: './user-details.css'
 })
@@ -16,6 +17,8 @@ export class UserDetails implements OnInit{
   user: any;
   error = '';
   loading = true;
+  userDetailsArray: { key: string; value: any }[] = [];
+  displayedColumns = ['key', 'value'];
 
   constructor(private route: ActivatedRoute, private userService: UsersService) {}
 
@@ -25,6 +28,20 @@ export class UserDetails implements OnInit{
     this.userService.getUserByID(this.userId).subscribe({
       next: (user) => {
         this.user = user;
+
+        if (this.user) {
+          this.userDetailsArray = [
+            { key: 'Username', value: this.user.name },
+            { key: 'Email', value: this.user.email },
+            { key: 'Phone', value: this.user.phone || '—' },
+            { key: 'Website', value: this.user.website },
+            { key: 'Company', value: this.user.company?.name || '—' },
+            { key: 'Street', value: this.user.address?.street || '—' },
+            { key: 'City', value: this.user.address?.city || '—' },
+            { key: 'Zip code', value: this.user.address?.zipcode || '—' }
+          ];
+        }
+
         this.loading = false;
       },
       error: (err) => {
