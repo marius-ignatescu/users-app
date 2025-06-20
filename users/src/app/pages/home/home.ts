4b6c6844e39a8d65ee660d86cbff35100b90728e
Marius Ignatescu <marius@descopera.org>
2025-06-20 17:06:47 +0300

    Display in Dashboard number of users, companies and the number of users per each company

--- users/src/app/pages/home/home.ts
+++ users/src/app/pages/home/home.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { ContextualNavigationBar } from "../../components/contextual-navigation-bar/contextual-navigation-bar";
+import { UsersStore } from '../../store/user-store';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterModule, ContextualNavigationBar],
+  imports: [RouterLink, RouterModule, ContextualNavigationBar, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
+
 export class Home {
-  title = signal('Home');
+  usersStore = inject(UsersStore);
+
+  totalUsers$ = this.usersStore.totalUsers$;
+  totalCompanies$ = this.usersStore.totalCompanies$;
+  usersPerCompany$ = this.usersStore.usersPerCompany$;
+
+  companyKeys(obj: { [key: string]: number }): string[] {
+    return Object.keys(obj);
+  }
 }
--- users/src/app/store/user-store.ts
+++ users/src/app/store/user-store.ts
@@ -2,7 +2,7 @@ import { signal, computed } from '@angular/core';
 import { UserItem } from '../model/user.type';
 import { createStore } from '@ngneat/elf';
 import { withEntities, addEntities, deleteEntities, selectAllEntities, selectEntity } from '@ngneat/elf-entities';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 const store = createStore(
   { name: 'users' },
@@ -14,6 +14,30 @@ export class UsersStore {
   private loaded = signal(false);
   isLoaded = computed(() => this.loaded());
   
+  totalUsers$: Observable<number> = this.users$.pipe(
+    map(users => users.length)
+  );
+
+  totalCompanies$: Observable<number> = this.users$.pipe(
+    map(users => {
+      const uniqueCompanies = new Set(
+        users.map(user => user.company?.name).filter(Boolean)
+      );
+      return uniqueCompanies.size;
+    })
+  );
+
+  usersPerCompany$: Observable<{ [company: string]: number }> = this.users$.pipe(
+    map(users => {
+      const map: { [company: string]: number } = {};
+      users.forEach(user => {
+        const company = user.company?.name || 'Unknown';
+        map[company] = (map[company] || 0) + 1;
+      });
+      return map;
+    })
+  );
+
   loadUsers(users: UserItem[]): void {
     store.update(
       addEntities(users)
